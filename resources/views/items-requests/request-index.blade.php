f5211fdd2ffb4af2889075d1d30edec22b0524cc
eddywm <eddywmdev@gmail.com>
2018-01-28 15:30:30 +0300

    Item Request Process : Showing the item image on the left
    Got rid of legal notices

--- resources/views/items-requests/request-index.blade.php
+++ resources/views/items-requests/request-index.blade.php
@@ -32,84 +32,107 @@
                 <div class="row">
 
                     <div class="col-md-6">
-                        <div class="panel panel-primary">
-                            <div class="panel-heading">
-                                DETAILS OF THE ITEM YOU ARE REQUESTING
-                            </div>
-                            <div class="panel-body">
-                                <div class="table-responsive" >
-                                    <table class="table table-bordered table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th>ITEM PROPERTY</th><th>ITEM VALUE</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <td> NAME</td> <td class="item_value_column">{{ $item->name }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td> SERIAL NUMBER</td> <td class="item_value_column">{{ $item->serial_number }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td> IDENTIFIER TAG</td> <td class="item_value_column">{{ $item->identifier }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td> PRICE</td> <td class="item_value_column">{{ "USD ".number_format( $item->price,2,'.',',') }}</td>
-                                        </tr>
+
+                        <h3>{{ $item->name }} . Image</h3>
+
+                        <a
+                                class="example-image-link"
+                                href="{{ isset($item->photo_url)? asset('storage/'.substr($item->photo_url,7)) : asset('assets/images/No_image_available.png') }}"
+                                data-lightbox="example-1">
+
+                            <img
+                                    src="{{ isset($item->photo_url)? asset('storage/'.substr($item->photo_url,7)) : asset('assets/images/No_image_available.png') }}"
+                                    class="img-thumbnail item-img-show example-image" alt="{{ $item->name }}" />
+                        </a>
+
+                        {{--<div class="panel panel-primary">--}}
+                            {{--<div class="panel-heading">--}}
+                                {{--DETAILS OF THE ITEM YOU ARE REQUESTING--}}
+                            {{--</div>--}}
 
 
-                                        <tr>
-                                            <td> TIME SPAN </td> <td class="item_value_column"> {{  $item->timeSpanObject()['hours']." hrs ".$item->timeSpanObject()['days']." days ".$item->timeSpanObject()['months']." months "
-                                 .$item->timeSpanObject()['years']." years" }}</td>
-                                        </tr>
 
-                                        <tr>
-                                            <td> CATEGORY</td> <td class="item_value_column">{{ $item->itemCategory->name }}</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td> CONDITION</td> <td class="item_value_column">{{ $item->itemCondition->name }}</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td> MODEL NUMBER</td> <td>{{ $item->model_number }}</td>
-                                        </tr>
+                            {{--<div class="panel-body">--}}
 
 
-                                        <tr>
-                                            <td>ITEM LOCATION</td>
-                                            <td>{{ $item->location }}</td>
-                                        </tr>
+
+                                {{--<div class="table-responsive" >--}}
+                                    {{--<table class="table table-bordered table-striped">--}}
+                                        {{--<thead>--}}
+                                        {{--<tr>--}}
+                                            {{--<th>ITEM PROPERTY</th><th>ITEM VALUE</th>--}}
+                                        {{--</tr>--}}
+                                        {{--</thead>--}}
+                                        {{--<tbody>--}}
+                                        {{--<tr>--}}
+                                            {{--<td> NAME</td> <td class="item_value_column">{{ $item->name }}</td>--}}
+                                        {{--</tr>--}}
+                                        {{--<tr>--}}
+                                            {{--<td> SERIAL NUMBER</td> <td class="item_value_column">{{ $item->serial_number }}</td>--}}
+                                        {{--</tr>--}}
+                                        {{--<tr>--}}
+                                            {{--<td> IDENTIFIER TAG</td> <td class="item_value_column">{{ $item->identifier }}</td>--}}
+                                        {{--</tr>--}}
+                                        {{--<tr>--}}
+                                            {{--<td> PRICE</td> <td class="item_value_column">{{ "USD ".number_format( $item->price,2,'.',',') }}</td>--}}
+                                        {{--</tr>--}}
 
 
-                                        </tbody>
+                                        {{--<tr>--}}
+                                            {{--<td> TIME SPAN </td> <td class="item_value_column"> {{  $item->timeSpanObject()['hours']." hrs ".$item->timeSpanObject()['days']." days ".$item->timeSpanObject()['months']." months "--}}
+                                 {{--.$item->timeSpanObject()['years']." years" }}</td>--}}
+                                        {{--</tr>--}}
+
+                                        {{--<tr>--}}
+                                            {{--<td> CATEGORY</td> <td class="item_value_column">{{ $item->itemCategory->name }}</td>--}}
+                                        {{--</tr>--}}
+
+                                        {{--<tr>--}}
+                                            {{--<td> CONDITION</td> <td class="item_value_column">{{ $item->itemCondition->name }}</td>--}}
+                                        {{--</tr>--}}
+
+                                        {{--<tr>--}}
+                                            {{--<td> MODEL NUMBER</td> <td>{{ $item->model_number }}</td>--}}
+                                        {{--</tr>--}}
 
 
-                                    </table>
-
-                                    @if(isset($itemAccessories))
-
-                                        <ul class="list-group">
-                                            <li class="list-group-item list-group-item-heading list-group-item-info">
-                                                <span class="badge">{{  count( $itemAccessories) }}</span>
-                                                Item Accessories
-                                            </li>
-                                            @foreach($itemAccessories as $itemAccessory)
-                                                <li class="list-group-item">{{ $itemAccessory->name }}</li>
-                                            @endforeach
-                                        </ul>
-
-                                    @endif
+                                        {{--<tr>--}}
+                                            {{--<td>ITEM LOCATION</td>--}}
+                                            {{--<td>{{ $item->location }}</td>--}}
+                                        {{--</tr>--}}
 
 
-                            </div> <!--table responsive-->
+                                        {{--</tbody>--}}
 
-                        </div>
-                        <div class="panel-footer">
-                        </div>
+
+                                    {{--</table>--}}
+
+                                    {{--@if(isset($itemAccessories))--}}
+
+                                        {{--<ul class="list-group">--}}
+                                            {{--<li class="list-group-item list-group-item-heading list-group-item-info">--}}
+                                                {{--<span class="badge">{{  count( $itemAccessories) }}</span>--}}
+                                                {{--Item Accessories--}}
+                                            {{--</li>--}}
+                                            {{--@foreach($itemAccessories as $itemAccessory)--}}
+                                                {{--<li class="list-group-item">{{ $itemAccessory->name }}</li>--}}
+                                            {{--@endforeach--}}
+                                        {{--</ul>--}}
+
+                                    {{--@endif--}}
+
+
+                            {{--</div> <!--table responsive-->--}}
+
+                        {{--</div>--}}
+                        {{--<div class="panel-footer">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+
+
                     </div>
-                    </div>
+
+
                     <div class="col-md-6">
 
 
@@ -132,6 +155,89 @@
                                 ITEM REQUEST DETAILS
                             </div>
 
+
+                                {{--<div class="panel-heading">--}}
+                                    {{--DETAILS OF THE ITEM YOU ARE REQUESTING--}}
+                                {{--</div>--}}
+
+
+
+
+
+
+
+                                    <div class="table-responsive" >
+                                        <table class="table table-bordered table-striped">
+                                            <thead>
+                                            <tr>
+                                                <th>ITEM PROPERTY</th><th>ITEM VALUE</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td> NAME</td> <td class="item_value_column">{{ $item->name }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td> SERIAL NUMBER</td> <td class="item_value_column">{{ $item->serial_number }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td> IDENTIFIER TAG</td> <td class="item_value_column">{{ $item->identifier }}</td>
+                                            </tr>
+
+                                           {{-- <tr>
+                                                <td> PRICE</td> <td class="item_value_column">{{ "USD ".number_format( $item->price,2,'.',',') }}</td>
+                                            </tr>
+
+
+                                            <tr>
+                                                <td> TIME SPAN </td> <td class="item_value_column"> {{  $item->timeSpanObject()['hours']." hrs ".$item->timeSpanObject()['days']." days ".$item->timeSpanObject()['months']." months "
+                                 .$item->timeSpanObject()['years']." years" }}</td>
+                                            </tr>--}}
+
+                                            <tr>
+                                                <td> CATEGORY</td> <td class="item_value_column">{{ $item->itemCategory->name }}</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td> CONDITION</td> <td class="item_value_column">{{ $item->itemCondition->name }}</td>
+                                            </tr>
+
+                                          {{--  <tr>
+                                                <td> MODEL NUMBER</td> <td>{{ $item->model_number }}</td>
+                                            </tr>--}}
+
+
+                                          {{--  <tr>
+                                                <td>ITEM LOCATION</td>
+                                                <td>{{ $item->location }}</td>
+                                            </tr>--}}
+
+
+                                            </tbody>
+
+
+                                        </table>
+
+                                        {{--@if(isset($itemAccessories))
+
+                                            <ul class="list-group">
+                                                <li class="list-group-item list-group-item-heading list-group-item-info">
+                                                    <span class="badge">{{  count( $itemAccessories) }}</span>
+                                                    Item Accessories
+                                                </li>
+                                                @foreach($itemAccessories as $itemAccessory)
+                                                    <li class="list-group-item">{{ $itemAccessory->name }}</li>
+                                                @endforeach
+                                            </ul>
+
+                                        @endif--}}
+
+
+                                    </div> <!--table responsive-->
+
+
+                            <hr>
+
                             {{--<form action="{{ route('request.post', $item->slug) }}" class="form" method="post">--}}
                             <form  action="{{ route('request.post', $item->slug) }}" method="POST">
 
@@ -139,9 +245,9 @@
 
                             <div class="panel-body">
 
-                                <p class="bg-info" style="padding: 15px; margin-bottom: 50px">
-                                    You have accepted terms and rights . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias aut debitis eligendi eos ex iste itaque optio quaerat qui rem reprehenderit repudiandae soluta totam veritatis voluptas voluptate, voluptatibus voluptatum.
-                                </p>
+                                {{--<p class="bg-info" style="padding: 15px; margin-bottom: 50px">--}}
+                                    {{--You have accepted terms and rights . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias aut debitis eligendi eos ex iste itaque optio quaerat qui rem reprehenderit repudiandae soluta totam veritatis voluptas voluptate, voluptatibus voluptatum.--}}
+                                {{--</p>--}}
 
                                 <div class="form-group">
                                     <label for="custom_accessories_selection">Accessory Selection</label>
